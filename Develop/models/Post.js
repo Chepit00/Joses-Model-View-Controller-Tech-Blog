e9835e3a8ba74c7fc4af46dc8f996bdb729631cd
Jose Silva <js38902@gmail.com>
2024-02-25 00:58:26 -0500

    added a few routes

--- Develop/models/Post.js
+++ Develop/models/Post.js
@@ -2,14 +2,42 @@ const { Model, DataTypes } = require("sequalize");
 const bcrypt = require("bcrypt");
 const sequalize = require("../config/connection");
 
-class Post extends Model { }
+class Post extends Model {}
 
-Post.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    
-});
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        module: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequalize,
+    timestamps: true,
+    freezTableName: true,
+    underscore: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Post;
